1e94af4e9da925d8f1e825086c1ce3be928e1d1e
Dilawarcs17 <97014139+Dilawarcs17@users.noreply.github.com>
2022-01-28 16:33:09 +0500

    first commit

--- code/new_main.js
+++ code/new_main.js
@@ -10,24 +10,23 @@ function fethc_data(url, bindfunc, row_Identifier) {
     }
     xhr.send();
 }
-
-
 // function for html 
-function htmlbody(logo, nameoflogo) {
-    return `
-    <div class="col-md-3 col-lg-3 col-sm-3">
-    <img src="${logo}" height="240" width="240">
-    <p style="color: white; text-align:center">${nameoflogo}</p>
-    </div>`
+function htmlbody(logo, nameoflogo, row_Identifier) {
+    $.get('temp.html', function (data) {
+        let nwData = data.replace('image', logo);
+        nwData = nwData.replace('data', nameoflogo);
+        document.getElementById(`${row_Identifier}`).innerHTML += nwData;
+    })
 }
 //function binding
 function bindfunc(calling_data, row_Identifier) {
     for (var i in calling_data) {
         var logo = calling_data[i].strMealThumb;
         var nameoflogo = calling_data[i].strMeal;
-        document.getElementById(`${row_Identifier}`).innerHTML += htmlbody(logo, nameoflogo);
+        htmlbody(logo, nameoflogo, row_Identifier);
     }
 }
+
 fethc_data("latest_meals.json", bindfunc, "latest_meal")
 fethc_data("Popular_Ingredients.json", bindfunc, "popular_ing")
 fethc_data("random_meals.json", bindfunc, "random_meal")
